7b581f9a064cae578899e13143f5f99cbc389603
chra-odoo <chra@odoo.com>
2026-01-15 11:00:09 -0800

    [ADD] example of Props

---
+++ chra_sale/static/src/components/child/child.js
@@ -0,0 +1,8 @@
+import { Component } from "@odoo/owl";
+
+export class Child extends Component {
+    static template = "chra_sale.Child";
+    static props = {
+        title: { type: String },
+    };
+}
--- chra_sale/static/src/components/example/example.js
+++ chra_sale/static/src/components/example/example.js
@@ -1,8 +1,17 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { standardWidgetProps } from "@web/views/widgets/standard_widget_props";
+import { Child } from "../child/child";
 
 export class Example extends Component {
     static template = "chra_sale.Example";
+    static components = {
+        Child,
+    };
+    static props = {
+        ...standardWidgetProps, // This includes the props for the standard widget
+        "*": true, // Allow any additional props to be passed through
+    };
 }
 
 registry.category("view_widgets").add("example", { component: Example });
