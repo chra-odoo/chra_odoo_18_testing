e0b360ce1e4d5a7b59f3f9ce2f155d4ad3ee9dc2
chra-odoo <chra@odoo.com>
2026-01-15 16:25:22 -0800

    [ADD] example of Ref hook

--- chra_sale/static/src/components/child/child.js
+++ chra_sale/static/src/components/child/child.js
@@ -1,4 +1,4 @@
-import { Component, onWillStart, onWillDestroy } from "@odoo/owl";
+import { Component, onWillStart, onWillDestroy, useRef } from "@odoo/owl";
 
 export class Child extends Component {
     static template = "chra_sale.Child";
@@ -10,11 +10,16 @@ export class Child extends Component {
     };
 
     setup() {
+        this.myInput = useRef("myInput");
         onWillStart(() => {
             console.log("Child component will start!");
         });
         onWillDestroy(() => {
             console.log("Child component will destroy!");
         });
+    }
+
+    focusMyInput() {
+        this.myInput.el.focus();
     }
 }
